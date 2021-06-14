71f7a833724979f72e73ecdf7cb1e2323741ba28
mcano <mcano@mobicorp.com>
2021-06-14 11:58:52 -0600

    mcano - auto generated git commit on: Mon Jun 14 11:58:52 CST 2021 \n files changed:  k8s/app.yaml |  6 +++---  src/app.sh   | 16 ++++++++++------  2 files changed, 13 insertions(+), 9 deletions(-)

--- src/app.sh
+++ src/app.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-: ${BUG:=""}
+: ${INSERT_BUG:=""}
 
 
 c=0
 while true;do
     c=$((c+1))
-    if [[ -z $BUG ]];then
+    if [[ -z ${INSERT_BUG} ]];then
         echo "[`date` ${c}s] Yes! you are using the good ✅ image"
-    elif [[ $BUG == "FATAL" ]];then
-        echo -e "[`date`] This is a bug 🐞 ! you are using the '-buggy' image \nHint: in order to fix it you may need edit/update the image and remove the -buggy from the image tag. 😉\n\n"
-        bug
+    elif [[ ${INSERT_BUG} == *"FATAL"* ]];then
+        echo "[$c] Entering a FATAL bug waiting to reach it..."
+        if [[ $c == 5 ]];then
+            echo -e "[`date`] FATAL This is a bug 🐞 ! you are using the '-buggy' image \nHint: in order to fix it you may need edit/update the image and remove the -buggy from the image tag. 😉\n\n"
+            bug
+            exit 100
+        fi
         sleep 0.5
     else
-        bug || true
         echo -e "[`date`] This is a bug 🐞 ! you are using the '-buggy' image \nHint: in order to fix it you may need edit/update the image and remove the -buggy from the image tag. 😉\n\n"
+        bug || true
         sleep 0.5
     fi
     sleep 1
