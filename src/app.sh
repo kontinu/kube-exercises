7e9dc99b0d71af8b6ef87254ffd444a7aab96153
mcano <mcano@mobicorp.com>
2021-06-15 06:04:38 -0600

    mcano - auto generated git commit on: Tue Jun 15 06:04:38 CST 2021 \n files changed:  src/Dockerfile | 3 ++-  src/app.sh     | 7 ++++---  2 files changed, 6 insertions(+), 4 deletions(-)

--- src/app.sh
+++ src/app.sh
@@ -6,19 +6,20 @@
 c=0
 while true;do
     c=$((c+1))
-    if [[ -z ${INSERT_BUG} ]];then
+    if [[ -z ${INSERT_BUG} ]] || [[ ${INSERT_BUG} == "no" ]]  || [[ ${INSERT_BUG} == "NO" ]] ; then
         echo "[`date` ${c}s] Yes! you are using the good ✅ image"
     elif [[ ${INSERT_BUG} == *"FATAL"* ]];then
         echo "[$c] Entering a FATAL bug waiting to reach it..."
         if [[ $c == 5 ]];then
-            echo -e "[`date`] FATAL This is a bug 🐞 ! you are using the '-buggy' image \nHint: in order to fix it you may need edit/update the image and remove the -buggy from the image tag. 😉\n\n"
+            echo -e "[`date`] FATAL This is a bug 🐞 ! you are using the '-buggy' image \nHint: perhaps remove the environment variable INSERT_BUG. 😉\n\n"
             bug
             exit 100
         fi
         sleep 0.5
     else
-        echo -e "[`date`] This is a bug 🐞 ! you are using the '-buggy' image \nHint: in order to fix it you may need edit/update the image and remove the -buggy from the image tag. 😉\n\n"
+        echo -e "[`date`] This is a bug 🐞 ! you are using the '-buggy' image \nHint: in order to fix it you may need edit/update the image 😉"
         bug || true
+        echo -e "\n"
         sleep 0.5
     fi
     sleep 1
